472562e0394c015c5537e6a470563c12fb80cd90
Brayshen <1947118818@qq.com>
2019-08-21 13:06:19 +0800

    还是修改了下

--- src/views/Searchlist/searchlist.vue
+++ src/views/Searchlist/searchlist.vue
@@ -7,7 +7,7 @@
       </a>
       <a href="#" class="search-right">
         <em class="icon iconfont icon-fangdajing"></em>
-        <span>请输入关键词搜索,修改下</span>
+        <span>请输入关键词搜索,小生儿~</span>
       </a>
       <ul class="search-type">
         <li
